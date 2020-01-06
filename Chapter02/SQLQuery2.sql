dd5089620f40e23854b62ebb26b23290ab52c715
Transfiguracion <Transfiguracion.Ryan@con-way.com>
2020-01-06 16:05:34 -0500

    Chapter 3 Page 81 -- Added two queries
    - Query1 sets up Northwind database
    - Query2 is the query sandbox
    - TODO (Other computer): set up Northwind database and set up connections.

---
+++ Chapter02/SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿WITH CTE_EmpRegion AS
+(
+	SELECT EmployeeID, MAX(RegionID) RegionID
+	FROM EmployeeTerritories et
+		INNER JOIN territories t ON t.TerritoryID = et.TerritoryID
+	GROUP BY EmployeeID
+),
+CTE_Sales AS
+(
+	SELECT RegionID, SUM(Quantity * UnitPrice) TotalSales
+	FROM Orders o
+		INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+		INNER JOIN CTE_EmpRegion er ON o.EmployeeID = er.EmployeeID
+	GROUP BY RegionID
+)
+SELECT s.RegionID, r.RegionDescription, s.TotalSales
+FROM CTE_Sales s INNER JOIN Region r on s.RegionID=r.RegionID
+ORDER BY s.RegionID
